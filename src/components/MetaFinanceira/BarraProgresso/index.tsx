1c9d3b129e0004c0363d7d0e3df03fbf2bb23cfa
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-12-22 17:30:42 -0300

    ✨ feat: conecta BarraProgresso ao progresso da store
    Integra o componente BarraProgresso ao MobX utilizando observer e useStore, exibindo o progresso da meta calculado na usuarioStore.

--- src/components/MetaFinanceira/BarraProgresso/index.tsx
+++ src/components/MetaFinanceira/BarraProgresso/index.tsx
@@ -1,14 +1,18 @@
+import { observer } from 'mobx-react'
+import { useStore } from '@/mobx/useStore'
 import { BarraContainer, StyledBarraProgresso, TextoProgresso } from './styled'
 
-const BarraProgresso = () => {
-    const progressoMeta = 50
+const BarraProgresso = observer(() => {
+    const { usuarioStore } = useStore()
 
     return (
         <BarraContainer>
-            <StyledBarraProgresso $progresso={progressoMeta} />
-            <TextoProgresso>{progressoMeta}%</TextoProgresso>
+            <StyledBarraProgresso $progresso={usuarioStore.progressoMeta} />
+            <TextoProgresso>
+                {usuarioStore.progressoMeta.toFixed(2)}%
+            </TextoProgresso>
         </BarraContainer>
     )
-}
+})
 
 export default BarraProgresso
