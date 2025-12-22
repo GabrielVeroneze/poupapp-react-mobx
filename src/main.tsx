486919a38e8c34439efce26969c6479f18bdf86f
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-12-22 16:08:51 -0300

    ♻️ refactor: ajusta import do StoreProvider
    Atualiza o caminho de importação do StoreProvider para refletir
    a nova organização dos arquivos MobX.

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { StoreProvider } from '@/mobx/StoreContext'
+import { StoreProvider } from '@/mobx/StoreProvider'
 import GlobalStyle from '@/styles/Global.js'
 import AppRoutes from '@/routes'
 
