7416f038f8111724247ecb32b376079f224d59c2
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-12-23 17:11:24 -0300

    ♻️ refactor: remove contas mockadas da Home
    Remove os dados mockados de contas da página Home e ajusta
    o componente Contas para ser renderizado sem props,
    preparando o consumo direto da contasStore.

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,4 @@
 import { Container, Movimentacoes, Orcamento } from './styled'
-import type { ContaBancaria } from '@/types/ContaBancaria'
 import BarraLateral from '@/components/BarraLateral'
 import BarraPesquisa from '@/components/BarraPesquisa'
 import SaudacaoUsuario from '@/components/SaudacaoUsuario'
@@ -8,12 +7,6 @@ import MetaFinanceira from '@/components/MetaFinanceira'
 import Transacoes from '@/components/Transacoes'
 import Contas from '@/components/Contas'
 import BalancoFinanceiro from '@/components/BalancoFinanceiro'
-
-const contas: ContaBancaria[] = [
-    { id: 1, banco: 'Anybank', saldo: 1500 },
-    { id: 2, banco: 'Bytebank', saldo: 2500 },
-    { id: 3, banco: 'Swiftbank', saldo: 3200 },
-]
 
 function Home() {
     return (
@@ -27,7 +20,7 @@ function Home() {
             </Orcamento>
             <Movimentacoes>
                 <Transacoes />
-                <Contas contas={contas} />
+                <Contas />
             </Movimentacoes>
             <BalancoFinanceiro />
         </Container>
