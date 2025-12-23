9d526c14511b1f8bfb0f8175e6af03efe7ae5619
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-12-23 16:37:23 -0300

    ✨ feat: integra TransacaoModal às stores MobX
    Conecta o TransacaoModal à transacoesStore e usuarioStore,
    adicionando novas transações e atualizando o orçamento diário
    ao submeter o formulário, com tipagem baseada em NovaTransacao.

--- src/components/Transacoes/TransacaoModal/index.tsx
+++ src/components/Transacoes/TransacaoModal/index.tsx
@@ -1,4 +1,8 @@
 import { useState } from 'react'
+import { observer } from 'mobx-react'
+import { useStore } from '@/mobx/useStore'
+import type { NovaTransacao } from '@/types/NovaTransacao'
+import type { TipoTransacao } from '@/types/TransacaoDados'
 import CampoTexto from '@/components/CampoTexto'
 import Fieldset from '@/components/Fieldset'
 import Form from '@/components/Form'
@@ -13,108 +17,116 @@ interface TransacaoModalProps {
     onCloseModal: () => void
 }
 
-const TransacaoModal = ({ isOpen, onCloseModal }: TransacaoModalProps) => {
-    const [novaTransacao, setNovaTransacao] = useState({
-        nome: '',
-        valor: 0,
-        tipo: '',
-        categoria: '',
-        data: '',
-    })
+const TransacaoModal = observer(
+    ({ isOpen, onCloseModal }: TransacaoModalProps) => {
+        const { transacoesStore, usuarioStore } = useStore()
+        const [novaTransacao, setNovaTransacao] = useState<NovaTransacao>({
+            nome: '',
+            valor: 0,
+            tipo: 'receita',
+            categoria: '',
+            data: '',
+        })
 
-    const aoSubmeterFormModal = () => {
-        onCloseModal()
-    }
+        const aoSubmeterFormModal = () => {
+            transacoesStore.adicionaTransacao(novaTransacao)
+            usuarioStore.atualizaOrcamentoDiario(novaTransacao)
 
-    return (
-        <Modal
-            aoFechar={() => onCloseModal()}
-            titulo="Adicionar transação"
-            estaAberta={isOpen}
-            icon={<MoneyIcon />}
-            aoClicar={() => aoSubmeterFormModal()}
-        >
-            <Form>
-                <Fieldset>
-                    <Label htmlFor="nomeTransacao">Nome da transação</Label>
-                    <CampoTexto
-                        type="text"
-                        id="nomeTransacao"
-                        placeholder="Ex: Compra na padaria"
-                        value={novaTransacao.nome}
-                        onChange={(e) =>
-                            setNovaTransacao({
-                                ...novaTransacao,
-                                nome: e.target.value,
-                            })
-                        }
-                    />
-                </Fieldset>
-                <Fieldset>
-                    <Label htmlFor="valor">Valor</Label>
-                    <CampoTexto
-                        type="number"
-                        id="valor"
-                        placeholder="10"
-                        value={novaTransacao.valor}
-                        onChange={(e) =>
-                            setNovaTransacao({
-                                ...novaTransacao,
-                                valor: Number(e.target.value),
-                            })
-                        }
-                    />
-                </Fieldset>
-                <Fieldset>
-                    <Label htmlFor="tipo">Tipo</Label>
-                    <SelectGroup
-                        id="tipo"
-                        value={novaTransacao.tipo}
-                        onChange={(e) =>
-                            setNovaTransacao({
-                                ...novaTransacao,
-                                tipo: e.target.value,
-                            })
-                        }
-                    >
-                        <SelectOption value="">Selecione o tipo</SelectOption>
-                        <SelectOption value="receita">Receita</SelectOption>
-                        <SelectOption value="despesa">Despesa</SelectOption>
-                    </SelectGroup>
-                </Fieldset>
-                <Fieldset>
-                    <Label htmlFor="valor">Data</Label>
-                    <CampoTexto
-                        type="date"
-                        id="valor"
-                        placeholder="dd/mm/aaaa"
-                        value={novaTransacao.data}
-                        onChange={(e) =>
-                            setNovaTransacao({
-                                ...novaTransacao,
-                                data: e.target.value,
-                            })
-                        }
-                    />
-                </Fieldset>
-                <Fieldset>
-                    <Label htmlFor="categoria">Categoria</Label>
-                    <CampoTexto
-                        type="text"
-                        id="categoria"
-                        placeholder="Alimentação"
-                        value={novaTransacao.categoria}
-                        onChange={(e) =>
-                            setNovaTransacao({
-                                ...novaTransacao,
-                                categoria: e.target.value,
-                            })
-                        }
-                    />
-                </Fieldset>
-            </Form>
-        </Modal>
-    )
-}
+            onCloseModal()
+        }
+
+        return (
+            <Modal
+                aoFechar={() => onCloseModal()}
+                titulo="Adicionar transação"
+                estaAberta={isOpen}
+                icon={<MoneyIcon />}
+                aoClicar={() => aoSubmeterFormModal()}
+            >
+                <Form>
+                    <Fieldset>
+                        <Label htmlFor="nomeTransacao">Nome da transação</Label>
+                        <CampoTexto
+                            type="text"
+                            id="nomeTransacao"
+                            placeholder="Ex: Compra na padaria"
+                            value={novaTransacao.nome}
+                            onChange={(e) =>
+                                setNovaTransacao({
+                                    ...novaTransacao,
+                                    nome: e.target.value,
+                                })
+                            }
+                        />
+                    </Fieldset>
+                    <Fieldset>
+                        <Label htmlFor="valor">Valor</Label>
+                        <CampoTexto
+                            type="number"
+                            id="valor"
+                            placeholder="10"
+                            value={novaTransacao.valor}
+                            onChange={(e) =>
+                                setNovaTransacao({
+                                    ...novaTransacao,
+                                    valor: Number(e.target.value),
+                                })
+                            }
+                        />
+                    </Fieldset>
+                    <Fieldset>
+                        <Label htmlFor="tipo">Tipo</Label>
+                        <SelectGroup
+                            id="tipo"
+                            value={novaTransacao.tipo}
+                            onChange={(e) =>
+                                setNovaTransacao({
+                                    ...novaTransacao,
+                                    tipo: e.target.value as TipoTransacao,
+                                })
+                            }
+                        >
+                            <SelectOption value="">
+                                Selecione o tipo
+                            </SelectOption>
+                            <SelectOption value="receita">Receita</SelectOption>
+                            <SelectOption value="despesa">Despesa</SelectOption>
+                        </SelectGroup>
+                    </Fieldset>
+                    <Fieldset>
+                        <Label htmlFor="valor">Data</Label>
+                        <CampoTexto
+                            type="date"
+                            id="valor"
+                            placeholder="dd/mm/aaaa"
+                            value={novaTransacao.data}
+                            onChange={(e) =>
+                                setNovaTransacao({
+                                    ...novaTransacao,
+                                    data: e.target.value,
+                                })
+                            }
+                        />
+                    </Fieldset>
+                    <Fieldset>
+                        <Label htmlFor="categoria">Categoria</Label>
+                        <CampoTexto
+                            type="text"
+                            id="categoria"
+                            placeholder="Alimentação"
+                            value={novaTransacao.categoria}
+                            onChange={(e) =>
+                                setNovaTransacao({
+                                    ...novaTransacao,
+                                    categoria: e.target.value,
+                                })
+                            }
+                        />
+                    </Fieldset>
+                </Form>
+            </Modal>
+        )
+    },
+)
 
 export default TransacaoModal
