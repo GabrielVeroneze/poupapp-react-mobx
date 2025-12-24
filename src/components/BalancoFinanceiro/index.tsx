f10f288b3f9de7abffba9261b40ac7f6a5ec4d87
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-12-24 15:06:50 -0300

    ✨ feat: adiciona gráfico de pizza com Chart.js
    Integra Chart.js e react-chartjs-2 ao componente de balanço financeiro, registrando os módulos necessários e exibindo um gráfico de pizza para visualização dos gastos por categoria.

--- src/components/BalancoFinanceiro/index.tsx
+++ src/components/BalancoFinanceiro/index.tsx
@@ -1,14 +1,35 @@
+import {
+    Chart as ChartJS,
+    ArcElement,
+    BarElement,
+    CategoryScale,
+    Legend,
+    LinearScale,
+    Tooltip,
+} from 'chart.js'
+import { Pie } from 'react-chartjs-2'
 import { AreaChart } from './styled'
 import Cartao from '@/components/Cartao'
 import CartaoCabecalho from '@/components/Cartao/CartaoCabecalho'
 import CartaoCorpo from '@/components/Cartao/CartaoCorpo'
+
+ChartJS.register(
+    ArcElement,
+    BarElement,
+    CategoryScale,
+    Legend,
+    LinearScale,
+    Tooltip,
+)
 
 const BalancoFinanceiro = () => {
     return (
         <Cartao>
             <CartaoCabecalho>Gastos por categoria</CartaoCabecalho>
             <CartaoCorpo>
-                <AreaChart></AreaChart>
+                <AreaChart>
+                    <Pie />
+                </AreaChart>
             </CartaoCorpo>
         </Cartao>
     )
